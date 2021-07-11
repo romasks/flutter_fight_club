aba2b2c0d6645ad8c7020ffbe2c9b57dca607588
Roma S <roman.skaskevich@gmail.com>
2021-07-11 12:05:50 +0200

    Remove fixes from git

--- android/app/src/main/kotlin/com/romasks/flutter_fight_club/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.romasks.flutter_fight_club
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/ru/skillbranch/flutter_fight_club/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package ru.skillbranch.flutter_fight_club
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
